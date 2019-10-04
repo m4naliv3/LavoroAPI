91af730b09424ee072838566ce7124e25ef551be
Josie <josiah.james92@gmail.com>
2019-10-03 23:06:33 -0700

    login stuff and create a new account

--- LavoroAPI/Controllers/SignupController.cs
+++ LavoroAPI/Controllers/SignupController.cs
@@ -1,20 +1,39 @@
-﻿using System.Web.Http;
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Web.Http;
 
 namespace LavoroAPI.Controllers
 {
     public class SignupController : ApiController
     {
         // GET: api/Signup/{username}
-        public string Get(string username)
+        public HttpResponseMessage Get()
         {
-            return (DbManager.CheckUser(username)) ? "This username has already been taken" : "This username is available";
+            string stuff = true ? "This username has already been taken" : "This username is available";
+            var response = JsonConvert.SerializeObject(stuff);
+            return new HttpResponseMessage() { Headers = { }, Content = new StringContent(response) };
         }
 
         // POST: api/Authentication
-        public void Post([FromBody]string username, string password)
+        public void Post([FromBody]NewUser user)
         {
             // need to return something to the app
-            DbManager.CreateUser(username, password);
+            DbManager.CreateAccount(
+                user.Username, 
+                user.Password,
+                user.BusinessName,
+                user.Email,
+                user.Avatar
+            );
         }
+    }
+
+    public class NewUser
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string BusinessName { get; set; }
+        public string Email { get; set; }
+        public string Avatar { get; set; }
     }
 }
