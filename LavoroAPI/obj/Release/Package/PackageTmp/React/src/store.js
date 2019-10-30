764e28d956bced6aee899af81187da8bd401069c
Josie <josiah.james92@gmail.com>
2019-10-29 22:08:08 -0700

    attempting to merge into master

--- LavoroAPI/obj/Release/Package/PackageTmp/React/src/store.js
+++
@@ -1,4 +0,0 @@
-import Custom_Reducer from './reducers';
-import { createStore } from 'redux'
-
-export const store = createStore( Custom_Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() );
